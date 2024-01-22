e6f99a8ce4bba5d3a14ce92abdf40919fe49b422
nishan <nishanpradhan500@gmail.com>
2024-01-22 22:15:01 +0545

    function

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,15 @@ void main() {
   runApp(const MyApp());
 }
 
+int data = 5;
+String name = 'nishan';
+double height = 5.6;
+
+int userId = 6;
+void textitems() {
+  Text('hello');
+}
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -13,7 +22,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -36,25 +44,19 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
-
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-
         title: Text(widget.title),
       ),
       body: Center(
-
         child: Column(
-
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
