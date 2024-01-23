38e1f692ac1a9d33d700fd71a7c72ee007444079
nishan <nishanpradhan500@gmail.com>
2024-01-23 22:57:29 +0545

    day-3-row-column-widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:project_07/screen.dart';
 
 void main() {
   runApp(const MyApp());
+  //data typoe and varibles
+  int data = 5;
+  String name = 'nishan';
+  double height = 5.6;
+
+  int userId = 6;
+  if (userId == 6) {
+    print('UserId is $userId');
+    print('User is a human');
+    print('Name is $name');
+    print('Height is $height');
+  } else {
+    print('bot number: $data');
+    print('User is a robot');
+  }
+
+  textitems();
 }
 
-int data = 5;
-String name = 'nishan';
-double height = 5.6;
-
-int userId = 6;
 void textitems() {
-  Text('hello');
+  int age = 21;
+  print("age is $age");
 }
 
 class MyApp extends StatelessWidget {
@@ -25,55 +39,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      home: Mainscreen(),
     );
   }
 }
