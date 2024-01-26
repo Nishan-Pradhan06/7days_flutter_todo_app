20e6941cade33ce3414b60cae5da1c3257550939
nishan <nishanpradhan500@gmail.com>
2024-01-26 17:04:10 +0545

    removed unnecessary-items

--- lib/main.dart
+++ lib/main.dart
@@ -4,28 +4,28 @@ import 'package:project_07/screens/screen.dart';
 void main() {
   runApp(const MyApp());
   //data typoe and varibles
-  int data = 5;
-  String name = 'nishan';
-  double height = 5.6;
+  // int data = 5;
+  // String name = 'nishan';
+  // double height = 5.6;
 
-  int userId = 6;
-  if (userId == 6) {
-    debugPrint('UserId is $userId');
-    debugPrint('User is a human');
-    debugPrint('Name is $name');
-    debugPrint('Height is $height');
-  } else {
-    debugPrint('bot number: $data');
-    debugPrint('User is a robot');
-  }
+  // int userId = 6;
+  // if (userId == 6) {
+  //   debugPrint('UserId is $userId');
+  //   debugPrint('User is a human');
+  //   debugPrint('Name is $name');
+  //   debugPrint('Height is $height');
+  // } else {
+  //   debugPrint('bot number: $data');
+  //   debugPrint('User is a robot');
+  // }
 
-  textitems();
+  // textitems();
 }
 
-void textitems() {
-  int age = 21;
- debugPrint("age is $age");
-}
+// void textitems() {
+//   int age = 21;
+//  debugPrint("age is $age");
+// }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
